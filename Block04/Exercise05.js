801434cd6678d31ebf5d76df5d3dc09dd4bec714
GoranBCSStudent <git config --global user.email goran@barcelonacodeschool.com>
2017-11-06 20:55:46 +0100

    change11

--- Block04/Exercise05.js
+++ Block04/Exercise05.js
@@ -1,24 +1,31 @@
-var commaCnt = 0
-var questionCnt = 0
-var str = 'hello, how are you today? I am not bad and you?'
-for (var letter of str) {
-	if (letter == ',') {
-		commaCnt ++
-	}
-	if (letter == '?') {
-		questionCnt ++
-	}
-}
-var firstWord = 'there is'
-var commaWord = 'comma'
-if (commaCnt > 1) {
-	commaWord += 's'
-	firstWord = 'there are'
-}
-var questionWord = 'mark'
-if (questionCnt > 1) {
-	questionWord += 's'
-	firstWord = 'there are'
-}
+var arrString = []
+arrString.push('hello, how are you today? I am not bad and you?')
+arrString.push('hello, how, are, you today? I am not bad and you?')
 
-console.log(`${firstWord} ${commaCnt} ${commaWord} and ${questionCnt} question ${questionWord} in this sentence`)
+arrString.forEach(function(el) {
+	var commaCnt = 0
+	var questionCnt = 0	
+	var str = el
+	for (var letter of str) {
+		if (letter == ',') {
+			commaCnt ++
+		}
+		if (letter == '?') {
+			questionCnt ++
+		}
+	}
+	var firstCommaWord = 'there is'
+	var commaWord = 'comma'
+	if (commaCnt > 1) {
+		commaWord += 's'
+		firstCommaWord = 'there are'
+	}
+	var firstQuestionWord = 'there is'
+	var questionWord = 'mark'
+	if (questionCnt > 1) {
+		questionWord += 's'
+		firstQuestionWord = 'there are'
+	}
+
+	console.log(`${firstCommaWord} ${commaCnt} ${commaWord} and ${firstQuestionWord} ${questionCnt} question ${questionWord} in this sentence`)	
+})
--- Block04/Exercise11.js
+++ Block04/Exercise11.js
@@ -1,10 +1,16 @@
-var bool [] = 0
+var bool = []
 var maxCapacity = 5
-var arr  = ['mike','john',true, false,12,true,false]
+//var arr  = ['mike','john',true, false,12,true,false]
+var arr = ['mike','john',true, false,12,true,false, false,true,false,true,false]
 for (var el of arr) {
-	if (typeof el == 'bool') {
+	if (typeof el == 'boolean' && bool.length < maxCapacity) {
 		bool.push(el)
 	}
 }
-
+console.log(bool)
 // couldn't understand this exercise
+
+/*in this exercise you should make sure that the array bool doesn't get filled with more than 5 elements, I guess the fact that the array example didn't have more than 5 didn't help. (sorry about that...)
+I have now changed it to this :
+var arr = ['mike','john',true, false,12,true,false, false,true,false,true,false]
+so it should hopefully make more sense.*/
