59d08ad8accc5f55f4f0256f35cbf429b5dd3878
GoranBCSStudent <git config --global user.email goran@barcelonacodeschool.com>
2017-11-06 20:22:34 +0100

    change09

--- Block06/Exercise15.js
+++ Block06/Exercise15.js
@@ -1,8 +1,17 @@
-function sort(objIn, sortType, sortOrder) {	
+function sort(objIn, sortType, sortDirection) {	
 	var arr = []
-	for (var key in objIn) {
-		arr.push(objIn[key])
+	if (!sortType || !sortDirection) {
+		return('missing argument here!')
 	}
+	if (sortType == 'keys') {
+		for (var key in objIn) {
+			arr.push(key)
+		}
+	} else {
+		for (var key in objIn) {
+			arr.push(objIn[key])
+		}	
+	}	
 
 	var arrSorted = []	
 	while (arr.length > 0) {
@@ -17,26 +26,28 @@ function sort(objIn, sortType, sortOrder) {
 		arrSorted.push(arr[pos])
 		arr.splice(pos,1)		
 	}
-	
-	var i = 0
-	var newObj = {}
-	arrSorted.forEach(function (ele) {
-		newObj[i] = ele
-		i++
-	})
+		
+	var newObj = {}	
+	if (sortDirection == 'ascending') {
+		for (var i = 0; i < arrSorted.length; i++ ) {
+			newObj[i] = arrSorted[i]
+		}
+	} else {
+		for (var i = 0; i < arrSorted.length; i++ ) {
+			newObj[arrSorted.length - 1 - i] = arrSorted[i]
+		}
+	}	
+		
 	return newObj
 }
 
 var obj = {a: 1, b: 20, c: 3, d: 4, e: 1, f: 4}
-sort(obj,'keys','ascending')
+console.log(sort(obj,'keys','ascending'))
 //Object {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f"}
-sort(obj,'keys','descending')
+console.log(sort(obj,'keys','descending'))
 //Object {0: "f", 1: "e", 2: "d", 3: "c", 4: "b", 5: "a"}
-sort(obj,'values','ascending')
+console.log(sort(obj,'values','ascending'))
 //Object {0: 1, 1: 1, 2: 3, 3: 4, 4: 4, 5: 20}
-sort(obj,'values','descending')
+console.log(sort(obj,'values','descending'))
 //Object {0: 20, 1: 4, 2: 4, 3: 3, 4: 1, 5: 1}
-
-
-//obj = {a: 1, b: 20, c: 3, d: 4, e: 1, f: 4}
-//console.log(sort(obj))
+console.log(sort(obj,'values'))
