76fdc239ebe0944605d377cfd232a0f3afd675cc
GoranBCSStudent <git config --global user.email goran@barcelonacodeschool.com>
2017-11-07 23:19:04 +0100

    change12

--- Block07/Exercise03.js
+++ Block07/Exercise03.js
@@ -1,9 +1,29 @@
 var bankAccount = {}
 bankAccount.withdraw = amount => {
-	console.log(amount * 2)
+	bankAccount.balanceValue -= amount
 }
-bankAccount.withdraw = amount => {
-	console.log(amount * 2)
+bankAccount.deposit = amount => {
+	bankAccount.balanceValue += amount
 }
+bankAccount.balance = () => {
+	console.log(bankAccount.balanceValue)
+}
+bankAccount.set = (amount) => {
+	bankAccount.balanceValue = amount
+}
+bankAccount.clear = () => {
+	bankAccount.balanceValue = 0
+}
+bankAccount.balanceValue = 0
 
-bankAccount.withdraw(4)
+bankAccount.withdraw(2)
+bankAccount.withdraw(5)
+bankAccount.deposit(4)
+bankAccount.deposit(1)
+bankAccount.balance()
+
+bankAccount.clear()
+bankAccount.balance()
+
+bankAccount.set(10)
+bankAccount.balance()
---
+++ Block07/Exercise04.js
@@ -0,0 +1,26 @@
+var bankAccount = function(amount) {
+	this.balanceValue = amount
+
+	this.withdraw = amount => {
+		this.balanceValue -= amount
+	}
+	this.deposit = amount => {
+		this.balanceValue += amount
+	}
+	this.balance = () => {
+		console.log(this.balanceValue)
+	}
+	this.set = (amount) => {
+		this.balanceValue = amount
+	}
+	this.clear = () => {
+		this.balanceValue = 0
+	}
+}
+
+var account = new bankAccount(10)
+account.withdraw(2)
+account.withdraw(5)
+account.deposit(4)
+account.deposit(1)
+account.balance()
